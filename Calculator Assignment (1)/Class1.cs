f61a4348b6b9aefcd39d79e7a9ae1232556bae01
osierra123 <93794335+osierra123@users.noreply.github.com>
2021-11-05 15:12:15 -0500

    Add files via upload

---
+++ Calculator Assignment (1)/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class Tipcalculator
+        
+    {
+        public Tipcalculator()
+        {
+            //Get the bill amount from the user
+            Console.WriteLine("Enter bill amount");
+            string Bill = Console.ReadLine();
+            float amount = float.Parse(Bill);
+
+            //Validate the value from the user to make sure it is a number
+            try
+            {
+               amount = float.Parse(Console.ReadLine());
+            }
+                catch (Exception ex)
+            {
+                Console.WriteLine("Please enter a valid bill amount");
+            }
+
+            //Ask the user how much they want to tip - show 10,15,20 percent options
+            Console.WriteLine("How much do you want to tip?");
+            string tip = Console.ReadLine();
+            float money = float.Parse(tip);
+            money = money / 100;
+
+            //Calculate tip amount from the bill amount and the percentage user chooses.
+            float total = amount * money;
+
+            //Show the final bill = bill amount + tip amount 
+            float charge = amount + total;
+            Console.WriteLine($"thank you for the tip. Your total is {charge}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Calculator Assignment (1)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get the bill amount from the user
+
+            //Validate the value from the user to make sure it is a number
+
+            //Ask the user how much they want to tip - show 10,15,20 percent options
+
+            //Calculate tip amount from the bill amount and the percentage user chooses.
+
+            //Show the final bill = bill amount + tip amount 
+            new Tipcalculator();
+        }
+        
+         
+    }
+}
